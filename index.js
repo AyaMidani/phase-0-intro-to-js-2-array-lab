94159c0ef8bd9107f03c4afddbde4272107df3fb
AyaMidani <61182291+AyaMidani@users.noreply.github.com>
2022-04-16 02:31:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-// Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+function beforeEach(){
+    cats.length = 0;
+    cats.push("Milo", "Otis", "Garfield");
+}
+function destructivelyAppendCat(name)
+{
+    cats.push(name)
+}
+function destructivelyPrependCat(name)
+{
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat()
+{
+    cats.pop()
+}
+function destructivelyRemoveFirstCat()
+{
+    cats.shift()
+}
+function appendCat(name)
+{
+    const NewCats = [...cats, name];
+    return NewCats
+}
+function prependCat(name)
+{
+    const NewCats = [name,...cats];
+    return NewCats
+}
+function removeLastCat()
+{
+    const NewCats = [...cats];
+    NewCats.pop()
+    return NewCats
+}
+function removeFirstCat()
+{
+    const NewCats = [...cats];
+    NewCats.shift()
+    return NewCats
+}
+destructivelyAppendCat("Ralph")
+beforeEach();
+destructivelyPrependCat("Bob")
+beforeEach();
+destructivelyRemoveLastCat()
+beforeEach();
+destructivelyRemoveFirstCat()
+beforeEach();
+appendCat("Simba")
+beforeEach();
+prependCat("Simba");
+beforeEach();
+removeLastCat();
+beforeEach();
+removeFirstCat();
+beforeEach();
+console.log(cats)
+
